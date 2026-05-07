a84fae9e67ac770961a5e6daaed6bc9686056ef1
Taohu <ht@qq.com>
2026-05-07 18:04:16 +0800

    修复代码问题: 子弹伤害浅拷贝 bug, 清理死代码与无效字段
    - fire_bullet/moon_bullet: final_attack 用 duplicate() 拷贝, 避免首次命中后污染基础伤害
    - 删除未被引用的 bullet_manage.gd (spawn 漏 add_child, 全代码无人调用)
    - 删除 fire.tres 中代码未读取的 perk_bonus 字段
    - moon_bullet: 调整字段顺序 (@export 在普通 var 前) 以满足 gdlint

--- scene/bullet/FireBullet/fire_bullet.gd
+++ scene/bullet/FireBullet/fire_bullet.gd
@@ -1,30 +1,33 @@
 extends Bullet
 @export var fire_strength = 100.0
-@export var hit_box : CollisionShape2D
+@export var hit_box: CollisionShape2D
 var fire_strength_decrease_time = 0.08
 var fire_strength_decrease_hit = 1.0
-var velocity : Vector2
+var velocity: Vector2
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	super()
-	speed = init_speed*(0.5+randf())
-	fire_strength = fire_strength * (0.7+0.6*randf())
+	speed = init_speed * (0.5 + randf())
+	fire_strength = fire_strength * (0.7 + 0.6 * randf())
+
 
 func _physics_process(delta: float) -> void:
-	fire_strength -= speed*delta*fire_strength_decrease_time*fire_strength_decrease_hit
-	fire_strength_decrease_hit=max(fire_strength_decrease_hit-delta*10,1)
-	if fire_strength<20:
+	fire_strength -= speed * delta * fire_strength_decrease_time * fire_strength_decrease_hit
+	fire_strength_decrease_hit = max(fire_strength_decrease_hit - delta * 10, 1)
+	if fire_strength < 20:
 		queue_free()
-	scale.x=fire_strength/50.0
-	scale.y=fire_strength/50.0
+	scale.x = fire_strength / 50.0
+	scale.y = fire_strength / 50.0
+
 
 func _on_area_entered(area: Area2D) -> void:
 	if area is HitboxComponent and area.can_be_damaged:
-		var final_attack = attack
-		final_attack.attack_damage=attack.attack_damage*fire_strength/100.0
+		var final_attack = attack.duplicate()
+		final_attack.attack_damage = attack.attack_damage * fire_strength / 100.0
 		area.damage(final_attack)
-		fire_strength_decrease_hit+=5
+		fire_strength_decrease_hit += 5
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
--- scene/bullet/MoonBullet/moon_bullet.gd
+++ scene/bullet/MoonBullet/moon_bullet.gd
@@ -1,47 +1,53 @@
 extends Bullet
-var chixushijian : float
-var xuanzhuansudu : float
-@export var tupian : Sprite2D
-@export var fazhen : Sprite2D
-@export var panding : CollisionShape2D
-@export var guangzhu : Array[Sprite2D]
-@export var shanghaijiange : float
-var process : float = 0.0
+@export var tupian: Sprite2D
+@export var fazhen: Sprite2D
+@export var panding: CollisionShape2D
+@export var guangzhu: Array[Sprite2D]
+@export var shanghaijiange: float
+var chixushijian: float
+var xuanzhuansudu: float
+var process: float = 0.0
+
 
 func _ready() -> void:
 	super()
-	chixushijian=0
-	
+	chixushijian = 0
+
+
 func _process(delta: float) -> void:
 	#旋转动画
-	chixushijian+=delta
-	xuanzhuansudu+=delta/2
-	tupian.rotate(xuanzhuansudu*delta)
-	var tupian_roration=tupian.rotation
-	var cnt=0
+	chixushijian += delta
+	xuanzhuansudu += delta / 2
+	tupian.rotate(xuanzhuansudu * delta)
+	var tupian_roration = tupian.rotation
+	var cnt = 0
 	for i in guangzhu:
-		var guangzhu_roration=tupian_roration+cnt*PI/3
-		var position1 = fazhen.position + Vector2(fazhen.get_rect().size.x/3,0).rotated(guangzhu_roration)*fazhen.scale
-		i.position=position1
-		i.position.y-=32*i.scale.y
-		cnt+=1
+		var guangzhu_roration = tupian_roration + cnt * PI / 3
+		var position1 = (
+			fazhen.position
+			+ Vector2(fazhen.get_rect().size.x / 3, 0).rotated(guangzhu_roration) * fazhen.scale
+		)
+		i.position = position1
+		i.position.y -= 32 * i.scale.y
+		cnt += 1
+
 
 func _physics_process(delta: float) -> void:
 	#伤害间隔
-	process+=delta
+	process += delta
 	if panding.disabled:
-		panding.set_deferred("disabled",false)
+		panding.set_deferred("disabled", false)
 	else:
 		if process > shanghaijiange:
-			process=0
-			panding.set_deferred("disabled",true)
+			process = 0
+			panding.set_deferred("disabled", true)
+
 
 func _on_area_entered(area: Area2D) -> void:
 	if area is HitboxComponent and area.can_be_damaged:
-		var final_attack=attack
-		if chixushijian>5:
-			final_attack.attack_damage=attack.attack_damage*2
+		var final_attack = attack.duplicate()
+		if chixushijian > 5:
+			final_attack.attack_damage = attack.attack_damage * 2
 		else:
-			final_attack.attack_damage=attack.attack_damage*(1+chixushijian/5)
+			final_attack.attack_damage = attack.attack_damage * (1 + chixushijian / 5)
 		area.damage(final_attack)
-			
